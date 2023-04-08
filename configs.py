ecaab42cc29582964b1cb7f43598a06ed1000d0d
Mrkk002 <127919334+Mrkk002@users.noreply.github.com>
2023-04-08 22:48:12 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -12,14 +12,14 @@ logging.basicConfig(
 
 
 class Config(object):
-    API_ID = int(os.environ.get("API_ID", ""))
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = int(os.environ.get("API_ID", "24505957"))
+    API_HASH = os.environ.get("API_HASH", "b9eae662a6dffd354d7bf18b239ad2bc")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6130789188:AAHt_IVBJn9xvF2kEGEkmK02hlkNGEhYyx8")
     DOWNLOAD_DIR = os.environ.get("DOWNLOAD_DIR", "./downloads")
     LOGGER = logging
-    OWNER_ID = int(os.environ.get("OWNER_ID", 1445283714))
-    PRO_USERS = list(set(int(x) for x in os.environ.get("PRO_USERS", "0").split()))
-    PRO_USERS.append(OWNER_ID)
+    OWNER_ID = int(os.environ.get("OWNER_ID", 1860415084))
+    PRO_USERS = list(set(int(x) for x in os.environ.get("PRO_USERS", "1").split()))
+    PRO_USERS.append(1860415084)
     MONGODB_URI = os.environ.get("MONGODB_URI", "")
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-100"))
     BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "False"))
